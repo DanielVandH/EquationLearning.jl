291f7c79eb9f88e54e738c1f645a32129f8ae8e7
DanielVandH <danj.vandenheuvel@gmail.com>
2022-02-14 13:59:32 +1000

    Package setup

--- src/EquationLearning.jl
+++ src/EquationLearning.jl
@@ -1,5 +1,45 @@
 module EquationLearning
 
-# Write your package code here.
+#####################################################################
+## Required packages
+#####################################################################
+
+using GaussianProcesses
+using Optim
+using MuladdMacro
+using LSODA
+using Dierckx
+using LaTeXStrings
+using DifferentialEquations
+using Sundials
+using FastGaussQuadrature
+using ODEInterfaceDiffEq
+using Plots
+using PreallocationTools
+using Printf
+using Random
+using LinearAlgebra
+using ForwardDiff
+using StatsPlots
+using Measures
+using LatinHypercubeSampling
+using StatsBase
+using DataFrames
+
+#####################################################################
+## Load files 
+#####################################################################
+
+include("gps.jl")
+include("pdes.jl")
+include("recipes.jl")
+include("bootstrapping.jl")
+include("optimisation.jl")
+include("utils.jl")
+include("structs.jl")
+
+#####################################################################
+## Export functions
+#####################################################################
 
 end
---
+++ src/gps.jl
@@ -0,0 +1,332 @@
+#####################################################################
+## File description: gps.jl 
+##
+## This script contains certain functions used for fitting Gaussian 
+## processes, primarily  leveraging the functions provided by 
+## GaussianProcesses.jl.
+##
+## The following functions are defined:
+##  - opt_restart!: Method for restarting the optimiser for the 
+##      Gaussian process with many initial guesses.
+##  - fit_GP: Fits a Gaussian process to some provided data.
+##  - dkxⱼ, d⁴kxᵢ²xⱼ², etc: Computing derivatives of the squared 
+##      exponential kernel, for use with compute_joint_GP.
+##  - compute_joint_GP: Computes the arrays required for the joint 
+##      Gaussian process [f, fₜ, fₓ, fₓₓ]. 
+##  - draw_gp!: Draw a random sample from a Gaussian process.
+##
+#####################################################################
+
+"""
+    opt_restart!(gp::GPBase; <keyword arguments>)
+
+Given a Gaussian process `gp::GPBase`, fit many new Gaussian processes with new initial estimates for the hyperparameters. The 
+initial estimates are chosen based on provided ranges for the hyperparameters and Latin hypercube sampling. See also [`fit_GP`](@ref).
+
+# Arguments 
+- 
+"""
+function opt_restart!(gp::GPBase; num_restarts::Int64 = 50, ℓₓ::Vector{Float64}, ℓₜ::Vector{Float64}, σ::Vector{Float64}, σₙ::Vector{Float64})
+    @assert length(ℓₓ) == length(ℓₜ) == length(σ) == length(σₙ) == 2 "The provided hyperparameters must be given as vectors of length 2 providing upper and lower bounds."
+    ## Form the design 
+    plan, _ = LHCoptim(num_restarts, length(GaussianProcesses.get_params(gp)), 1000)
+    # Scale the design into the provided intervals 
+    new_params = scaleLHC(plan, [(ℓₓ[1], ℓₓ[2]), (ℓₜ[1], ℓₜ[2]), (σ[1], σ[2]), (σₙ[1], σₙ[2])])'
+    # Optimise
+    obj_values = zeros(num_restarts)
+    for j = 1:num_restarts
+        try
+            @views GaussianProcesses.set_params!(gp, new_params[:, j])
+            GaussianProcesses.optimize!(gp)
+            obj_values[j] = gp.target
+        catch err
+            println(err)
+            obj_values[j] = -Inf
+        end
+    end
+    # Return optimal model 
+    opt_model = findmax(obj_values)[2]
+    @views GaussianProcesses.set_params!(gp, new_params[:, opt_model])
+    GaussianProcesses.optimize!(gp)
+    return nothing
+end
+
+"""
+    fit_GP(x, t, u; <keyword arguments>)
+
+Fits a Gaussian process with data `(x, t)` using the targets in `u`. 
+
+# Arguments 
+- `x`: The spatial data. 
+- `t`: The temporal data.
+- `u`: The targets corresponding to `(x, t)`.
+
+# Keyword Arguments 
+- `ℓₓ = -2.0`: The initial estimate of the spatial length scale, given on a log scale. 
+- `ℓₜ = -0.4`: The initial estimate of the temporal length scale, given on a log scale.
+- `σ = 7.0`: The initial estimate of the signal standard deviation, given on a log scale.
+- `σₙ = 2.0`: The initial estimate of the standard deviation of the observation noise, given on a log noise.
+- `num_restarts = 50`: Number of times to restart the optimiser. See [`Optimise_Restart!`](@ref).
+
+# Outputs 
+- `gp`: The fitted Gaussian process.
+
+# Extended help
+In this function we scale the data to be in [0, 1] to allow for the optimisation scheme to converge with 
+greater stability. The scaling used is `x ↤ (x - minimum(x))/(maximum(x) - minimum(x))` and similarly for `t`.
+"""
+function fit_GP(x, t, u; ℓₓ = log.([1e-4, 1.0]), ℓₜ = log.([1e-4, 1.0]), σ = log.([1e-1, 2std(u)]), σₙ = log.([1e-5, 2std(u)]), num_restarts = 50)
+    # Define the GP 
+    meanFunc = MeanZero()
+    covFunc = SE([mean(exp.(ℓₓ)), mean(exp.(ℓₜ))], mean(exp.(σ)))
+    # Check that the data are row vectors and that the target is a column vector
+    x = Matrix(vec(x)')
+    t = Matrix(vec(t)')
+    u = vec(u)
+    # Now normalise the data 
+    x_min = minimum(x)
+    x_max = maximum(x)
+    t_min = minimum(t)
+    t_max = maximum(t)
+    xx = @. (x - x_min) / (x_max - x_min)
+    tt = @. (t - t_min) / (t_max - t_min)
+    X = [xx; tt]
+    # Now fit the GP
+    gp = GPE(X, u, meanFunc, covFunc, mean(exp.(σₙ)))
+    ## Obtain num_restarts random estimates for the hyperparameters
+    opt_restart!(gp; num_restarts = num_restarts, ℓₓ, ℓₜ, σ, σₙ)
+    return gp
+end
+
+
+@doc "See [`compute_joint_GP`](@ref) for details." @inline function dkxⱼ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+    (x₁ - x₂) / ℓ₁^2
+end
+@doc "See [`compute_joint_GP`](@ref) for details." @inline @muladd function d²kxⱼ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+    -(ℓ₁ * ℓ₁ - x₁ * x₁ + 2x₁ * x₂ - x₂ * x₂) / ℓ₁^4
+end
+@doc "See [`compute_joint_GP`](@ref) for details." @inline function dktⱼ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+    (t₁ - t₂) / ℓ₂^2
+end
+@doc "See [`compute_joint_GP`](@ref) for details." @inline function dktᵢ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+    -(t₁ - t₂) / ℓ₂^2
+end
+@doc "See [`compute_joint_GP`](@ref) for details." @inline @muladd function d²ktᵢtⱼ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+    (ℓ₂ * ℓ₂ - t₁ * t₁ + 2t₁ * t₂ - t₂ * t₂) / ℓ₂^4
+end
+@doc "See [`compute_joint_GP`](@ref) for details." @inline function d²ktᵢxⱼ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+    -(t₁ - t₂) * (x₁ - x₂) / (ℓ₁^2 * ℓ₂^2)
+end
+@doc "See [`compute_joint_GP`](@ref) for details." @inline @muladd function d³tᵢxⱼ²(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+    (t₁ - t₂) * (ℓ₁ * ℓ₁ - x₁ * x₁ + 2x₁ * x₂ - x₂ * x₂) / (ℓ₁^4 * ℓ₂^2)
+end
+@doc "See [`compute_joint_GP`](@ref) for details." @inline function dkxᵢ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+    -(x₁ - x₂) / (ℓ₁^2)
+end
+@doc "See [`compute_joint_GP`](@ref) for details." @inline function d²kxᵢtⱼ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+    -(t₁ - t₂) * (x₁ - x₂) / (ℓ₁^2 * ℓ₂^2)
+end
+@doc "See [`compute_joint_GP`](@ref) for details." @inline @muladd function d²kxᵢxⱼ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+    (ℓ₁ * ℓ₁ - x₁ * x₁ + 2x₁ * x₂ - x₂ * x₂) / ℓ₁^4
+end
+@doc "See [`compute_joint_GP`](@ref) for details." @inline @muladd function d³kxᵢxⱼ²(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+    (x₁ - x₂) * (3ℓ₁ * ℓ₁ - x₁ * x₁ + 2x₁ * x₂ - x₂ * x₂) / ℓ₁^6
+end
+@doc "See [`compute_joint_GP`](@ref) for details." @inline @muladd function d²kxᵢ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+    -(ℓ₁ * ℓ₁ - x₁ * x₁ + 2x₁ * x₂ - x₂ * x₂) / ℓ₁^4
+end
+@doc "See [`compute_joint_GP`](@ref) for details." @inline @muladd function d³kxᵢ²tⱼ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+    -(t₁ - t₂) * (ℓ₁ * ℓ₁ - x₁ * x₁ + 2x₁ * x₂ - x₂ * x₂) / (ℓ₁^4 * ℓ₂^2)
+end
+@doc "See [`compute_joint_GP`](@ref) for details." @inline @muladd function d³kxᵢ²xⱼ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+    -(x₁ - x₂) * (3ℓ₁ * ℓ₁ - x₁ * x₁ + 2x₁ * x₂ - x₂ * x₂) / ℓ₁^6
+end
+@doc "See [`compute_joint_GP`](@ref) for details." @inline @muladd function d⁴kxᵢ²xⱼ²(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+    (3ℓ₁ * ℓ₁ * ℓ₁ * ℓ₁ - 6ℓ₁ * ℓ₁ * x₁ * x₁ + 12ℓ₁ * ℓ₁ * x₁ * x₂ - 6ℓ₁ * ℓ₁ * x₂ * x₂ + x₁ * x₁ * x₁ * x₁ - 4x₁ * x₁ * x₁ * x₂ + 6x₁ * x₁ * x₂ * x₂ - 4x₁ * x₂ * x₂ * x₂ + x₂ * x₂ * x₂ * x₂) / ℓ₁^8
+end
+
+"""
+    compute_joint_GP(gp, X̃; nugget = 1e-10)
+
+Computes the mean vector `μ` and Cholesky factor `L` such that `LLᵀ = Σ`, where 
+
+`` \\left[\\begin{array}{c} \\mathbf{f} \\\\ \\frac{\\partial\\mathbf f}{\\partial t} \\\\ \\frac{\\partial\\mathbf f}{\\partial x} \\\\\\frac{\\partial^2\\mathbf f}{\\partial x^2}\\end{array}\\right] \\sim \\mathcal N\\left(\\mathbf{\\mu}, \\mathbf{\\Sigma}\\right).``
+
+# Arguments 
+- `gp`: The fitted Gaussian process. 
+- `Xₛ`: Test data for the Gaussian process. 
+
+# Keyword Arguments 
+- `nugget = 1e-10`: The term to add to the diagonals of the covariance matrix incase the matrix is not positive definite.
+
+# Outputs 
+- `μ`: The mean vector. 
+- `L`: The Cholesky factor of the covariance matrix.
+
+# Extended help
+The covariance matrices are built without any attention to symmetry. This is done for readability purposes
+(the direction of this benefit being debatable...). The loops could be optimised by e.g. considering only the 
+upper triangular components. 
+
+The covariance matrices are built using separate functions for the derivatives of the kernel function (currently 
+only implemented for the squared exponential kernel). These functions are:
+
+- [`dkxⱼ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)`](@ref).
+- [`d²kxⱼ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)`](@ref).
+- [`dktⱼ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)`](@ref).
+- [`dktᵢ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)`](@ref).
+- [`d²ktᵢtⱼ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)`](@ref).
+- [`d²ktᵢxⱼ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)`](@ref).
+- [`d³tᵢxⱼ²(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)`](@ref).
+- [`dkxᵢ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)`](@ref).
+- [`d²kxᵢtⱼ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)`](@ref).
+- [`d²kxᵢxⱼ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)`](@ref).
+- [`d³kxᵢxⱼ²(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)`](@ref).
+- [`d²kxᵢ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)`](@ref).
+- [`d³kxᵢ²tⱼ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)`](@ref).
+- [`d³kxᵢ²xⱼ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)`](@ref).
+- [`d⁴kxᵢ²xⱼ²(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)`](@ref).
+
+In these functions, the derivatives are evaluated at `([x₁; t₁], [x₂; t₂])` with length scales 
+`(ℓ₁, ℓ₂)` for space and time, resectively. The functions are missing a multiplication by the 
+kernel value `cov(gp.kernel, [x₁; t₁], [x₂; t₂])` to allow or it to be done in place more easily. 
+Note also that in some of these functions, powers are written as producs, e.g. `x³ = x*x*x`, to allow 
+for the `@muladd` macro to work. See https://github.com/SciML/MuladdMacro.jl. Since the functions are 
+quite small, we use `@inline` on each function to encourage the compiler to inline the function 
+in the LLVM.
+
+You may get a `StackOverflowError` from this function. This may be related to https://github.com/JuliaLang/julia/issues/43242,
+in which case you can set the number of BLAS threads to 1:
+```julia-repl 
+julia> ccall((:openblas_get_num_threads64_, Base.libblas_name), Cint, ()) # In case you need to remember how many threads you used prior
+julia> LinearAlgebra.BLAS.set_num_threads(1)
+```
+"""
+function compute_joint_GP(gp, X̃; nugget = 1e-10)
+    # Extract the original data matrix 
+    X = gp.x
+    nₓnₜ = size(X̃, 2)
+    NM = size(X, 2)
+    ℓ₁, ℓ₂ = gp.kernel.iℓ2 .^ (-1 / 2)
+
+    # The primary covariance matrices 
+    Kyy = gp.cK
+    Kyf = cov(gp.kernel, X, X̃)
+    Kfy = cov(gp.kernel, X̃, X)
+    Kff = cov(gp.kernel, X̃, X̃)
+
+    # The remaining covariance matrices between data and test data
+    Ky_∂f∂t = copy(Kyf)
+    Ky_∂f∂x = copy(Kyf)
+    Ky_∂²f∂x² = copy(Kyf)
+    K∂f∂t_y = copy(Kfy)
+    K∂f∂x_y = copy(Kfy)
+    K∂²f∂x²_y = copy(Kfy)
+    for j = 1:nₓnₜ
+        for i = 1:NM
+            x₁, t₁, x₂, t₂ = X[1, i], X[2, i], X̃[1, j], X̃[2, j]
+            Ky_∂f∂t[i, j] *= dktⱼ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+            Ky_∂f∂x[i, j] *= dkxⱼ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+            Ky_∂²f∂x²[i, j] *= d²kxⱼ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+        end
+    end
+    for j = 1:NM
+        for i = 1:nₓnₜ
+            x₁, t₁, x₂, t₂ = X̃[1, i], X̃[2, i], X[1, j], X[2, j]
+            K∂f∂t_y[i, j] *= dktᵢ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+            K∂f∂x_y[i, j] *= dkxᵢ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+            K∂²f∂x²_y[i, j] *= d²kxᵢ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+        end
+    end
+
+    # Covariance matrices between test data 
+    Kf_∂f∂t = copy(Kff)
+    Kf_∂f∂x = copy(Kff)
+    Kf_∂²f∂x² = copy(Kff)
+    K∂f∂t_f = copy(Kff)
+    K∂f∂t_∂f∂t = copy(Kff)
+    K∂f∂t_∂f∂x = copy(Kff)
+    K∂f∂t_∂²f∂x² = copy(Kff)
+    K∂f∂x_f = copy(Kff)
+    K∂f∂x_∂f∂t = copy(Kff)
+    K∂f∂x_∂f∂x = copy(Kff)
+    K∂f∂x_∂²f∂x² = copy(Kff)
+    K∂²f∂x²_f = copy(Kff)
+    K∂²f∂x²_∂f∂t = copy(Kff)
+    K∂²f∂x²_∂f∂x = copy(Kff)
+    K∂²f∂x²_∂²f∂x² = copy(Kff)
+    for j = 1:nₓnₜ
+        for i = 1:nₓnₜ
+            x₁, t₁, x₂, t₂ = X̃[1, i], X̃[2, i], X̃[1, j], X̃[2, j]
+            Kf_∂f∂t[i, j] *= dktⱼ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+            Kf_∂f∂x[i, j] *= dkxⱼ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+            Kf_∂²f∂x²[i, j] *= d²kxⱼ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+            K∂f∂t_f[i, j] *= dktᵢ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+            K∂f∂t_∂f∂t[i, j] *= d²ktᵢtⱼ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+            K∂f∂t_∂f∂x[i, j] *= d²ktᵢxⱼ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+            K∂f∂t_∂²f∂x²[i, j] *= d³tᵢxⱼ²(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+            K∂f∂x_f[i, j] *= dkxᵢ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+            K∂f∂x_∂f∂t[i, j] *= d²kxᵢtⱼ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+            K∂f∂x_∂f∂x[i, j] *= d²kxᵢxⱼ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+            K∂f∂x_∂²f∂x²[i, j] *= d³kxᵢxⱼ²(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+            K∂²f∂x²_f[i, j] *= d²kxᵢ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+            K∂²f∂x²_∂f∂t[i, j] *= d³kxᵢ²tⱼ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+            K∂²f∂x²_∂f∂x[i, j] *= d³kxᵢ²xⱼ(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+            K∂²f∂x²_∂²f∂x²[i, j] *= d⁴kxᵢ²xⱼ²(x₁, t₁, x₂, t₂, ℓ₁, ℓ₂)
+        end
+    end
+
+    # Now combine the results 
+    Σ₁₁ = Symmetric(Kyy.mat)
+    Σ₁₂ = hcat(Kyf, Ky_∂f∂t, Ky_∂f∂x, Ky_∂²f∂x²)
+    Σ₂₁ = vcat(Kfy, K∂f∂t_y, K∂f∂x_y, K∂²f∂x²_y)
+    Σ₂₂ = Symmetric([Kff Kf_∂f∂t Kf_∂f∂x Kf_∂²f∂x²
+        K∂f∂t_f K∂f∂t_∂f∂t K∂f∂t_∂f∂x K∂f∂t_∂²f∂x²
+        K∂f∂x_f K∂f∂x_∂f∂t K∂f∂x_∂f∂x K∂f∂x_∂²f∂x²
+        K∂²f∂x²_f K∂²f∂x²_∂f∂t K∂²f∂x²_∂f∂x K∂²f∂x²_∂²f∂x²])
+
+    # Compute the mean vector 
+    μ = Σ₂₁ * gp.alpha
+
+    # Compute the covariance matrix and Cholesky factor 
+    Σ = Symmetric(Σ₂₂ - Σ₂₁ * (Σ₁₁ \ Σ₁₂))
+    local L # So it's kept outside of try
+    try
+        Σ, chol = GaussianProcesses.make_posdef!(Σ; nugget = nugget)
+        L = chol.L
+    catch
+        Σvar = diag(Σ)
+        for i = 1:size(Σ, 2)
+            Σ[i, i] += nugget * Σvar[i] # add nugget to correlation matrix instead!
+        end
+        Σ, chol = GaussianProcesses.make_posdef!(Σ; nugget = nugget)
+        L = chol.L
+    end
+    # Return 
+    return μ, L
+end
+
+"""
+    draw_gp!(F, μ, L, z, ℓz) 
+    
+Draws a random sample from a Gaussian process with mean `μ` and 
+covariance matrix `Σ = LLᵀ` corresponding to `z ∼ N(0, I)`.
+
+# Arguments
+- `F`: Cache array used for storing the random sample.
+- `μ`: The mean vector.
+- `L`: The Cholesky factor of the covariance matrix.
+- `z`: The random sample from `z ∼ N(0, I)`.
+- `ℓz`: Cache array for storing the result of the matrix-vector product `Lz`.
+
+# Outputs 
+The random sample is updated in-place into `F`.
+
+# Extended help
+The function draws a random sample by noting that if `X ∼ N(μ, Σ)`, then 
+`X ∼ μ + Lz`, where `z ∼ N(0, I)` and `LLᵀ = Σ`.
+"""
+@inline function draw_gp!(F, μ, L, z, ℓz)
+    F .= μ + mul!(ℓz, LowerTriangular(L), z)
+    return nothing
+end
