d9abff82a492ca2f8194d9aa39d9ea8f8cba0d12
DanielVandH <danj.vandenheuvel@gmail.com>
2022-02-14 23:37:06 +1000

    Moved BootResults to bottom so that we don't reference yet-to-be-defined structs

--- src/structs.jl
+++ src/structs.jl
@@ -17,55 +17,6 @@
 #####################################################################
 
 """
-    struct BootResults
-
-Structure for storing bootstrapping results. See [`bootstrap_gp`](@ref).
-
-# Fields 
-- `delayBases`: The estimated delay parameters. Each column corresponds to a single bootstrap iteration.
-- `diffusionBases`: The estimated diffusion parameters. Each column corresponds to a single bootstrap iteration.
-- `reactionBases`: The estimated reaction parameters. Each column corresponds to a single bootstrap iteration.
-- `gp`: The fitted Gaussian process. See [`fit_gp`](@ref).
-- `zvals`: The simulated normal variables from `N(0, I)` used for drawing from the Gaussian process `gp`. See [`draw_gp!`](@ref).
-- `Xₛ`: The test matrix for the bootstrapping grid data, given in the scale [0, 1].
-- `Xₛⁿ`: The unscaled form of `Xₛ`.
-- `bootₓ`: The spatial bootstrapping grid.
-- `bootₜ`: The temporal bootstrapping grid.
-- `T`: The delay function, given in the form `T(t, α, T_params)`.
-- `D`: The diffusion function, given in the form `D(u, β, D_params)`.
-- `D′`: The derivative of the diffusion function, given in the form `D′(u, β, D_params)`.
-- `R`: The reaction function, given in the form `R(u, γ, R_params)`.
-- `D_params`: Parameters for the diffusion function.
-- `R_params`: Parameters for the reaction function. 
-- `T_params`: Parameters for the delay function.
-- `μ`: The mean vector for the joint Gaussian process. See also [`compute_joint_GP`](@ref).
-- `L`: The Cholesky factor for the joint Gaussian process. See also [`compute_joint_GP`](@ref).
-"""
-struct BootResults
-    delayBases::Array{Float64}
-    diffusionBases::Array{Float64}
-    reactionBases::Array{Float64}
-    gp::GPBase
-    zvals::Array{Float64}
-    Xₛ::Array{Float64}
-    Xₛⁿ::Array{Float64}
-    bootₓ::Vector{Float64}
-    bootₜ::Vector{Float64}
-    T::Function
-    D::Function
-    D′::Function
-    R::Function
-    D_params
-    R_params
-    T_params
-    μ::Vector{Float64}
-    L::LowerTriangular{Float64}
-    gp_setup::GP_Setup
-    bootstrap_setup::Bootstrap_Setup 
-    pde_setup::PDE_Setup
-end
-
-"""
     mutable struct GP_Setup{T}
 
 Setup for the Gaussian processes. See also [`fit_GP`](@ref) and [`bootstrap_gp`](@ref).
@@ -231,4 +182,53 @@ function PDE_Setup(x, t;
         end
     end
     return PDE_Setup(meshPoints, LHS, RHS, finalTime, δt, alg)
+end
+
+"""
+    struct BootResults
+
+Structure for storing bootstrapping results. See [`bootstrap_gp`](@ref).
+
+# Fields 
+- `delayBases`: The estimated delay parameters. Each column corresponds to a single bootstrap iteration.
+- `diffusionBases`: The estimated diffusion parameters. Each column corresponds to a single bootstrap iteration.
+- `reactionBases`: The estimated reaction parameters. Each column corresponds to a single bootstrap iteration.
+- `gp`: The fitted Gaussian process. See [`fit_gp`](@ref).
+- `zvals`: The simulated normal variables from `N(0, I)` used for drawing from the Gaussian process `gp`. See [`draw_gp!`](@ref).
+- `Xₛ`: The test matrix for the bootstrapping grid data, given in the scale [0, 1].
+- `Xₛⁿ`: The unscaled form of `Xₛ`.
+- `bootₓ`: The spatial bootstrapping grid.
+- `bootₜ`: The temporal bootstrapping grid.
+- `T`: The delay function, given in the form `T(t, α, T_params)`.
+- `D`: The diffusion function, given in the form `D(u, β, D_params)`.
+- `D′`: The derivative of the diffusion function, given in the form `D′(u, β, D_params)`.
+- `R`: The reaction function, given in the form `R(u, γ, R_params)`.
+- `D_params`: Parameters for the diffusion function.
+- `R_params`: Parameters for the reaction function. 
+- `T_params`: Parameters for the delay function.
+- `μ`: The mean vector for the joint Gaussian process. See also [`compute_joint_GP`](@ref).
+- `L`: The Cholesky factor for the joint Gaussian process. See also [`compute_joint_GP`](@ref).
+"""
+struct BootResults
+    delayBases::Array{Float64}
+    diffusionBases::Array{Float64}
+    reactionBases::Array{Float64}
+    gp::GPBase
+    zvals::Array{Float64}
+    Xₛ::Array{Float64}
+    Xₛⁿ::Array{Float64}
+    bootₓ::Vector{Float64}
+    bootₜ::Vector{Float64}
+    T::Function
+    D::Function
+    D′::Function
+    R::Function
+    D_params
+    R_params
+    T_params
+    μ::Vector{Float64}
+    L::LowerTriangular{Float64}
+    gp_setup::GP_Setup
+    bootstrap_setup::Bootstrap_Setup 
+    pde_setup::PDE_Setup
 end
