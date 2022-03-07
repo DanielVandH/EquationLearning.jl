05f1e2dee6dbc6587fbc04ae97f3e861a69055e7
DanielVandH <danj.vandenheuvel@gmail.com>
2022-03-07 21:32:01 +1000

    Add some intermediate functions for computing function values to make creating plots manually easier

--- src/plot_results.jl
+++ src/plot_results.jl
@@ -5,8 +5,10 @@
 ## equation learning process.
 ##
 ## The following functions are defined:
+##  - density_values: Compute the densities for plotting density plots.
 ##  - density_results: Plots the densities of the estimates from the 
 ##      bootstrapping procedure.
+##  - curve_values: Compute the ribbon features for plotting functional forms.
 ##  - curve_results: Plots the learned functional forms for the parameter 
 ##      estimates from the bootstrapping procedure.
 ##  - pde_results: Plots the PDE solutions for each bootstrap iteration.
@@ -14,9 +16,84 @@
 #####################################################################
 
 """
-density_results(bgp::BootResults; <keyword arguments>)
+    density_values(bgp::BootResults; <keyword arguments>)
 
-    Plots the densities for the bootstrapping results in `bgp`.
+Computes the densities for the bootstrapping results in `bgp`.
+
+# Arguments 
+- `bgp`: A [`BootResults`](@ref) object which contains the results for the bootstrapping. 
+
+# Keyword Arguments 
+- `level = 0.05`: The significance level for computing the credible intervals for the parameter values. 
+- `fontsize = 23`: Font size for the plots (to be used in [`plot_aes!`](@ref)).
+- `delay_scales = nothing`: Values that multiply the individual delay parameters.
+- `diffusion_scales = nothing`: Values that multiply the individual diffusion parameters. 
+- `reaction_scales = nothing`: Values that multiply the individual reaction parameters.
+
+# Outputs 
+- `trv`: Delay densities.
+- `dr`: Diffusion densities. 
+- `rr`: Reaction densities. 
+- `tt`: Number of delay parameters.
+- `d`: Number of diffusion parameters.
+- `r`: Number of reaction parameters. 
+- `delayCIs`: Confidence intervals for the delay parameters. 
+- `diffusionCIs`: Confidence intervals for the diffusion parameters. 
+- `reactionCIS`: Confidence intervals for the reaction parameters.
+"""
+function density_values(bgp::BootResults; level = 0.05, delay_scales = nothing, diffusion_scales = nothing, reaction_scales = nothing)
+    # Start with delay
+    trv = copy(bgp.delayBases)
+    if !isnothing(delay_scales)
+        try
+            trv .*= vec(delay_scales)
+        catch
+            trv = trv * delay_scales
+        end
+    end
+    tt = size(trv, 1)
+    delayCIs = zeros(tt, 2)
+    quantiles = [level / 2 1 - level / 2]
+    @inbounds @views for j = 1:tt
+        delayCIs[j, :] .= quantile(trv[j, :], quantiles)[1:2] # Do [1:2] to transpose it so that we can broadcast
+    end
+
+    # Now work on diffusion 
+    dr = copy(bgp.diffusionBases)
+    if !isnothing(diffusion_scales)
+        try
+            dr .*= vec(diffusion_scales)
+        catch
+            dr = dr * diffusion_scales
+        end
+    end
+    d = size(dr, 1)
+    diffusionCIs = zeros(d, 2)
+    @inbounds @views for j = 1:d
+        diffusionCIs[j, :] .= quantile(dr[j, :], quantiles)[1:2]
+    end
+
+    # Now do reaction 
+    rr = copy(bgp.reactionBases)
+    if !isnothing(reaction_scales)
+        try
+            rr .*= vec(reaction_scales)
+        catch
+            rr = rr * reaction_scales
+        end
+    end
+    r = size(rr, 1)
+    reactionCIs = zeros(r, 2)
+    @inbounds @views for j = 1:r
+        reactionCIs[j, :] .= quantile(rr[j, :], quantiles)[1:2]
+    end
+    return trv, dr, rr, tt, d, r, delayCIs, diffusionCIs, reactionCIs
+end
+
+"""
+    density_results(bgp::BootResults; <keyword arguments>)
+
+Plots the densities for the bootstrapping results in `bgp`.
 
 # Arguments 
 - `bgp`: A [`BootResults`](@ref) object which contains the results for the bootstrapping. 
@@ -37,53 +114,10 @@ density_results(bgp::BootResults; <keyword arguments>)
 - `reactionDensityFigure`: A figure of plots containing a density plot for each reaction parameter.
 """
 function density_results(bgp::BootResults; level = 0.05, fontsize = 23, delay_scales = nothing, diffusion_scales = nothing, reaction_scales = nothing,
-        diffusion_resolution = (800, 800), reaction_resolution = (800, 800), delay_resolution = (800, 800))
-    # Start with delay
-    trv = copy(bgp.delayBases) 
-    if !isnothing(delay_scales)
-        try
-            trv .*= vec(delay_scales)
-        catch
-            trv = trv*delay_scales 
-        end
-    end
-    tt = size(trv, 1)
-    delayCIs = zeros(tt, 2)
-    quantiles = [level / 2 1 - level / 2]
-    @inbounds @views for j = 1:tt
-        delayCIs[j, :] .= quantile(trv[j, :], quantiles)[1:2] # Do [1:2] to transpose it so that we can broadcast
-    end
+    diffusion_resolution = (800, 800), reaction_resolution = (800, 800), delay_resolution = (800, 800))
 
-    # Now work on diffusion 
-    dr = copy(bgp.diffusionBases)
-    if !isnothing(diffusion_scales)
-        try
-            dr .*= vec(diffusion_scales)
-        catch 
-            dr = dr*diffusion_scales
-        end
-    end
-    d = size(dr, 1)
-    diffusionCIs = zeros(d, 2)
-    @inbounds @views for j = 1:d
-        diffusionCIs[j, :] .= quantile(dr[j, :], quantiles)[1:2]
-    end
-
-    # Now do reaction 
-    rr = copy(bgp.reactionBases)
-    if !isnothing(reaction_scales)
-        try
-            rr .*= vec(reaction_scales)
-        catch 
-            rr = rr*reaction_scales 
-        end
-    end
-    r = size(rr, 1)
-    reactionCIs = zeros(r, 2)
-    @inbounds @views for j = 1:r
-        reactionCIs[j, :] .= quantile(rr[j, :], quantiles)[1:2]
-    end
-
+    # Compute densities 
+    trv, dr, rr, tt, d, r, delayCIs, diffusionCIs, reactionCIs = density_values(bgp; level = level, delay_scales = delay_scales, diffusion_scales = diffusion_scales, reaction_scales = reaction_scales)
     # Pre-allocate the plots 
     delayDensityAxes = Vector{Axis}(undef, tt)
     diffusionDensityAxes = Vector{Axis}(undef, d)
@@ -131,26 +165,26 @@ function density_results(bgp::BootResults; level = 0.05, fontsize = 23, delay_sc
 end
 
 """
-    curve_results(bgp::BootResults; <keyword arguments>)
+    curve_values(bgp::BootResults; <keyword arguments>)
 
-Plots the learned functional forms along with confidence intervals for the bootstrapping results in `bgp`.
+Computes values for plotting the learned functional forms along with confidence intervals for the bootstrapping results in `bgp`.
 
 # Arguments 
 - `bgp`: A [`BootResults`](@ref) object which contains the results for the bootstrapping. 
     
 # Keyword Arguments 
 - `level = 0.05`: The significance level for computing the credible intervals for the parameter values. 
-- `fontsize = 23`: Font size for the plots (to be used in [`plot_aes!`](@ref)).
 - `x_scale = 1.0`: Value used for scaling the spatial data (and all other length units, e.g. for diffusion).
 - `t_scale = 1.0`: Value used for scaling the temporal data (and all other time units, e.g. for reaction).
 
 # Outputs
-There are no returned values from this function, but there plots created inside R using `RCall`:
-- `delayCurvePlots`: A plot containing the learned functional form for the delay function, along with an uncertainty ribbon.
-- `diffusionCurvePlots`: A plot containing the learned functional form for the diffusion function, along with an uncertainty ribbon.
-- `reactionCurvePlots`: A plot containing the learned functional form for the reaction function, along with an uncertainty ribbon.
+- `Tu_vals`: Ribbon features for the delay functions. 
+- `Du_vals`: Ribbon features for the diffusion functions.
+- `Ru_vals`: Ribbon features for the reaction functions.
+- `u_vals`: The density values used for computing the functions. 
+- `t_vals`: The time values used for computing the functions.
 """
-function curve_results(bgp::BootResults; level = 0.05, fontsize = 23, x_scale = 1.0, t_scale = 1.0)
+function curve_values(bgp::BootResults; level = 0.05, x_scale = 1.0, t_scale = 1.0)
     # Setup parameters and grid for evaluation
     trv = bgp.delayBases
     dr = bgp.diffusionBases
@@ -167,8 +201,8 @@ function curve_results(bgp::BootResults; level = 0.05, fontsize = 23, x_scale = 
     Ru = zeros(num_u, B)
     @views @inbounds for j = 1:B
         Tu[:, j] .= bgp.T.(t_vals, Ref(trv[:, j]), Ref(bgp.T_params)) # Use Ref() so that we only broadcast over the first argument.
-        Du[:, j] .= bgp.D.(u_vals, Ref(dr[:, j]), Ref(bgp.D_params)) * x_scale^2/t_scale
-        Ru[:, j] .= bgp.R.(u_vals, Ref(rr[:, j]), Ref(bgp.R_params)) / (x_scale * t_scale)
+        Du[:, j] .= bgp.D.(u_vals, Ref(dr[:, j]), Ref(bgp.D_params)) * x_scale^2 / t_scale
+        Ru[:, j] .= bgp.R.(u_vals, Ref(rr[:, j]), Ref(bgp.R_params)) / t_scale
     end
 
     # Find lower/upper values for confidence intervals, along with mean curves
@@ -176,26 +210,82 @@ function curve_results(bgp::BootResults; level = 0.05, fontsize = 23, x_scale = 
     Du_vals = compute_ribbon_features(Du; level = level)
     Ru_vals = compute_ribbon_features(Ru; level = level)
 
+    # Return 
+    return Tu_vals, Du_vals, Ru_vals, u_vals, t_vals
+end
+
+"""
+    curve_results(bgp::BootResults; <keyword arguments>)
+
+Plots the learned functional forms along with confidence intervals for the bootstrapping results in `bgp`.
+
+# Arguments 
+- `bgp`: A [`BootResults`](@ref) object which contains the results for the bootstrapping. 
+    
+# Keyword Arguments 
+- `level = 0.05`: The significance level for computing the credible intervals for the parameter values. 
+- `fontsize = 23`: Font size for the plots (to be used in [`plot_aes!`](@ref)).
+- `x_scale = 1.0`: Value used for scaling the spatial data (and all other length units, e.g. for diffusion).
+- `t_scale = 1.0`: Value used for scaling the temporal data (and all other time units, e.g. for reaction).
+
+# Outputs
+- `delayCurvePlots`: A plot containing the learned functional form for the delay function, along with an uncertainty ribbon.
+- `diffusionCurvePlots`: A plot containing the learned functional form for the diffusion function, along with an uncertainty ribbon.
+- `reactionCurvePlots`: A plot containing the learned functional form for the reaction function, along with an uncertainty ribbon.
+"""
+function curve_results(bgp::BootResults; level = 0.05, fontsize = 23, x_scale = 1.0, t_scale = 1.0)
+    # Compute values 
+    Tu_vals, Du_vals, Ru_vals, u_vals, t_vals = curve_values(bgp; level = level, x_scale = x_scale, t_scale = t_scale)
+
     # Plot delay curves 
     delayCurvePlots = Figure(fontsize = fontsize)
     ax = Axis(delayCurvePlots[1, 1], xlabel = L"t", ylabel = L"T(t)", linewidth = 1.3, linecolor = :blue)
-    lines!(ax, t_vals*t_scale, Tu_vals[1])
-    band!(ax, t_vals*t_scale, Tu_vals[3], Tu_vals[2], color = (:blue, 0.35))
+    lines!(ax, t_vals * t_scale, Tu_vals[1])
+    band!(ax, t_vals * t_scale, Tu_vals[3], Tu_vals[2], color = (:blue, 0.35))
 
     # Plot the diffusion curves 
     diffusionCurvePlots = Figure(fontsize = fontsize)
     ax = Axis(diffusionCurvePlots[1, 1], xlabel = L"u", ylabel = L"D(u)", linewidth = 1.3, linecolor = :blue)
-    lines!(ax, u_vals/x_scale^2, Du_vals[1])
-    band!(ax, u_vals/x_scale^2, Du_vals[3], Du_vals[2], color = (:blue, 0.35))
+    lines!(ax, u_vals / x_scale^2, Du_vals[1])
+    band!(ax, u_vals / x_scale^2, Du_vals[3], Du_vals[2], color = (:blue, 0.35))
 
     # Plot the reaction curves 
     reactionCurvePlots = Figure(fontsize = fontsize)
     ax = Axis(reactionCurvePlots[1, 1], xlabel = L"u", ylabel = L"R(u)", linewidth = 1.3, linecolor = :blue)
-    lines!(ax, u_vals/x_scale^2, Ru_vals[1])
-    band!(ax, u_vals/x_scale^2, Ru_vals[3], Ru_vals[2], color = (:blue, 0.35))
+    lines!(ax, u_vals / x_scale^2, Ru_vals[1])
+    band!(ax, u_vals / x_scale^2, Ru_vals[3], Ru_vals[2], color = (:blue, 0.35))
 
     # Return 
     return delayCurvePlots, diffusionCurvePlots, reactionCurvePlots
+end
+
+"""
+    function pde_values(solns_all, bgp::BootResults; level = 0.05)
+
+Plots the solutions to the PDEs corresponding to the bootstrap samples of [`bootstrap_gp`](@ref) using 
+the computed solutins in [`boot_pde_solve`](@ref).
+
+# Arguments
+- `solns_all`: The solutions to the PDEs for each bootstrap iteration. 
+
+# Keyword Arguments 
+- `level = 0.05`: The significance level for computing the credible intervals for the parameter values. 
+
+# Outputs 
+- `soln_vals_mean`: Mean solution values. 
+- `soln_vals_lowers`: Lower confidence interval for solution values.
+- `soln_vals_upper`: Upper confidence interval for solution values.
+"""
+function pde_values(solns_all, bgp::BootResults; level = 0.05)
+    N = length(bgp.pde_setup.meshPoints)
+    M = length(bgp.pde_setup.δt)
+    soln_vals_mean = zeros(N, M)
+    soln_vals_lower = zeros(N, M)
+    soln_vals_upper = zeros(N, M)
+    for j = 1:M
+        soln_vals_mean[:, j], soln_vals_lower[:, j], soln_vals_upper[:, j] = compute_ribbon_features(solns_all[:, :, j]; level = level)
+    end
+    return soln_vals_mean, soln_vals_lower, soln_vals_upper
 end
 
 """
@@ -225,15 +315,9 @@ function pde_results(x_pde, t_pde, u_pde, solns_all, bgp::BootResults;
     colors = [:black, :blue, :red, :magenta, :green], level = 0.05, fontsize = 23,
     x_scale = 1.0, t_scale = 1.0)
     ## Setup
-    N = length(bgp.pde_setup.meshPoints)
     M = length(bgp.pde_setup.δt)
     @assert length(colors) == M "There must be as many provided colors as there are unique time values."
-    soln_vals_mean = zeros(N, M)
-    soln_vals_lower = zeros(N, M)
-    soln_vals_upper = zeros(N, M)
-    for j = 1:M
-        soln_vals_mean[:, j], soln_vals_lower[:, j], soln_vals_upper[:, j] = compute_ribbon_features(solns_all[:, :, j]; level = level)
-    end
+    soln_vals_mean, soln_vals_lower, soln_vals_upper = pde_values(solns_all, bgp; level = level)
 
     # Initiate axis 
     pdeSolutionPlots_BGP = Figure(fontsize = fontsize)
