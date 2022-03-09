59abba87f59b45d9a060913cca2a529ecb088a6a
DanielVandH <danj.vandenheuvel@gmail.com>
2022-03-09 16:43:18 +1000

    Basis bootstrapping study

--- VandenHeuvel2022_PaperCode/simulation_studies.jl
+++ VandenHeuvel2022_PaperCode/simulation_studies.jl
@@ -18,6 +18,7 @@ using LinearAlgebra         # For setting number of threads to prevent StackOver
 using Setfield              # For modifying immutable structs
 using Printf                # For the @sprintf command
 using KernelDensity         # For kernel density estimation
+using Sundials              # For CVODE_BDF
 
 #####################################################################
 ## Set some global parameters for plotting
@@ -810,3 +811,117 @@ end
 Legend(resultFigures[1:2, 4], [values(legendentries)...], [keys(legendentries)...], "Time (h)", orientation = :vertical, labelsize = fontsize, titlesize = fontsize, titleposition = :top)
 save("figures/simulation_study_final_quadratic_affine_diffusion_results.pdf", resultFigures, px_per_unit = 2)
 
+#####################################################################
+## Study IV: Quadratic diffusion model, 12,000 cells per well, basis function approach 
+#####################################################################
+Random.seed!(510384421)
+# Select the dataset 
+dat = assay_data[2]
+
+# Extract initial conditions
+x₀ = dat.Position[dat.Time.==0.0]
+u₀ = dat.AvgDens[dat.Time.==0.0]
+
+# Define the functions and parameters
+T = (t, α, p) -> 1.0
+D = (u, β, p) -> β[1] * p[2] + β[2] * p[3] * (u / p[1]) + β[3] * p[4] * (u / p[1])^2
+D′ = (u, β, p) -> β[2] * p[3] / p[1] + 2 * β[3] * p[4] * u / p[1]^2
+R = (u, γ, p) -> γ[1] * p[2] * u * (1.0 - u / p[1])
+R′ = (u, γ, p) -> γ[1] * p[2] - 2.0 * γ[1] * p[2] * u / p[1]
+α = Vector{Float64}([])
+β = [300.0, 1700.0, 3200.0] * t_scale / x_scale^2
+γ = [0.06] * t_scale
+T_params = Vector{Float64}([])
+D_params = [K, 1.0, 1.0, 1.0]
+R_params = [K, 1.0]
+
+# Generate the data 
+x, t, u, datgp = EquationLearning.generate_data(x₀, u₀, T, D, R, D′, R′, α, β, γ, δt, finalTime; N, LHS, RHS, alg, N_thin, num_restarts, D_params, R_params, T_params)
+x_pde = copy(x)
+t_pde = copy(t)
+u_pde = copy(u)
+
+# Compute the mean vector and Cholesky factor for the GP 
+σ = log.([1e-1, 2std(u)])
+σₙ = log.([1e-5, 2std(u)])
+gp, μ, L = EquationLearning.precompute_gp_mean(x, t, u, ℓₓ, ℓₜ, σ, σₙ, nugget, GP_Restarts, bootstrap_setup)
+gp_setup = EquationLearning.GP_Setup(u; ℓₓ, ℓₜ, σ, σₙ, GP_Restarts, μ, L, nugget, gp)
+
+# Define functions and parameters to try and learn 
+T = (t, α, p) -> 1.0
+D = [(u, p) -> 1.0, (u, p) -> u / p[1], (u, p) -> (u / p[1])^2]
+D′ = [(u, p) -> 0.0, (u, p) -> 1 / p[1], (u, p) -> 2u/p[1]^2]
+R = convert(Vector{Function}, [(u, p) -> u * (1.0 - u / p[1])])
+R′ = convert(Vector{Function}, [(u, p) -> 1.0 - 2u/p[1]])
+D_params = [K]
+R_params = [K]
+
+# Now do the bootstrapping 
+bootstrap_setup = @set bootstrap_setup.B = 200
+bgp = basis_bootstrap_gp(x, t, u, D, D′, R, R′; gp_setup, bootstrap_setup, pde_setup, D_params, R_params, verbose = false)
+pde_setup = @set pde_setup.alg = CVODE_BDF(linear_solver = :Band, jac_upper = 1, jac_lower = 1)
+pde_data = boot_pde_solve(bgp, x_pde, t_pde, u_pde; ICType = "data")
+pde_gp = boot_pde_solve(bgp, x_pde, t_pde, u_pde; ICType = "gp")
+
+# Now look at the results
+unscaled_β = β * x_scale^2 / t_scale
+unscaled_γ = γ / t_scale
+dr, rr, d, r, diffusionCIs, reactionCIs = EquationLearning.density_values(bgp; level = 0.05, diffusion_scales = x_scale^2 / t_scale, reaction_scales = 1 / t_scale)
+resultFigures = Figure(fontsize = 17, resolution = (1600, 800))
+diffusionDensityAxes = Vector{Axis}(undef, d)
+reactionDensityAxes = Vector{Axis}(undef, r)
+for i = 1:d
+    diffusionDensityAxes[i] = Axis(resultFigures[1, i], xlabel = L"$\beta_%$i$ (μm²/h)", ylabel = "Probability density",
+        title = @sprintf("(%s): 95%% CI: (%.4g, %.4g)", alphabet[i], diffusionCIs[i, 1], diffusionCIs[i, 2]),
+        titlealign = :left)
+    densdat = KernelDensity.kde(dr[i, :])
+    vlines!(diffusionDensityAxes[i], unscaled_β[i], color = :red, linestyle = :dash)
+    in_range = minimum(dr[i, :]) .< densdat.x .< maximum(dr[i, :])
+    lines!(diffusionDensityAxes[i], densdat.x[in_range], densdat.density[in_range], color = :blue, linewidth = 3)
+    CI_range = diffusionCIs[i, 1] .< densdat.x .< diffusionCIs[i, 2]
+    band!(diffusionDensityAxes[i], densdat.x[CI_range], densdat.density[CI_range], zeros(count(CI_range)), color = (:blue, 0.35))
+end
+for i = 1:r
+    reactionDensityAxes[i] = Axis(resultFigures[1, i+3], xlabel = L"$\gamma_%$i$ (1/h)", ylabel = "Probability density",
+        title = @sprintf("(%s): 95%% CI: (%.4g, %.4g)", alphabet[i+3], reactionCIs[i, 1], reactionCIs[i, 2]),
+        titlealign = :left)
+    densdat = kde(rr[i, :])
+    vlines!(reactionDensityAxes[i], unscaled_γ[i], color = :red, linestyle = :dash)
+    in_range = minimum(rr[i, :]) .< densdat.x .< maximum(rr[i, :])
+    lines!(reactionDensityAxes[i], densdat.x[in_range], densdat.density[in_range], color = :blue, linewidth = 3)
+    CI_range = reactionCIs[i, 1] .< densdat.x .< reactionCIs[i, 2]
+    band!(reactionDensityAxes[i], densdat.x[CI_range], densdat.density[CI_range], zeros(count(CI_range)), color = (:blue, 0.35))
+end
+
+Du_vals, Ru_vals, u_vals = EquationLearning.curve_values(bgp; level = 0.05, x_scale = x_scale, t_scale = t_scale)
+diffusionAxis = Axis(resultFigures[2, 1], xlabel = L"$u$ (cells/μm²)", ylabel = L"$D(u)$ (μm²/h)", title = "(e): Diffusion curve", linewidth = 1.3, linecolor = :blue, titlealign = :left)
+lines!(diffusionAxis, u_vals / x_scale^2, Du_vals[1])
+band!(diffusionAxis, u_vals / x_scale^2, Du_vals[3], Du_vals[2], color = (:blue, 0.35))
+Dfnc = u -> evaluate_basis.(Ref(β), Ref(D), u, [K])
+lines!(diffusionAxis, u_vals / x_scale^2, Dfnc(u_vals) .* x_scale^2 / t_scale, color = :red, linestyle = :dash)
+Rfnc = u -> evaluate_basis.(Ref(γ), Ref(R), u, [K])
+reactionAxis = Axis(resultFigures[2, 2], xlabel = L"$u$ (cells/μm²)", ylabel = L"$R(u)$ (1/h)", title = "(f): Reaction curve", linewidth = 1.3, linecolor = :blue, titlealign = :left)
+lines!(reactionAxis, u_vals / x_scale^2, Ru_vals[1])
+band!(reactionAxis, u_vals / x_scale^2, Ru_vals[3], Ru_vals[2], color = (:blue, 0.35))
+lines!(reactionAxis, u_vals / x_scale^2, Rfnc(u_vals) / t_scale, color = :red, linestyle = :dash)
+
+soln_vals_mean, soln_vals_lower, soln_vals_upper = pde_values(pde_data, bgp)
+err_CI = error_comp(bgp, pde_data, x_pde, t_pde, u_pde)
+M = length(bgp.pde_setup.δt)
+dataAxis = Axis(resultFigures[2, 3], xlabel = L"$x$ (μm)", ylabel = L"$u(x, t)$ (cells/μm²)", title = @sprintf("(g): PDE curves with spline ICs\nError: (%.4g, %.4g)", err_CI[1], err_CI[2]), titlealign = :left)
+@views for j in 1:M
+    lines!(dataAxis, bgp.pde_setup.meshPoints * x_scale, soln_vals_mean[:, j] / x_scale^2, color = colors[j])
+    band!(dataAxis, bgp.pde_setup.meshPoints * x_scale, soln_vals_upper[:, j] / x_scale^2, soln_vals_lower[:, j] / x_scale^2, color = (colors[j], 0.35))
+    CairoMakie.scatter!(dataAxis, x_pde[t_pde.==bgp.pde_setup.δt[j]] * x_scale, u_pde[t_pde.==bgp.pde_setup.δt[j]] / x_scale^2, color = colors[j], markersize = 3)
+end
+soln_vals_mean, soln_vals_lower, soln_vals_upper = pde_values(pde_gp, bgp)
+err_CI = error_comp(bgp, pde_gp, x_pde, t_pde, u_pde)
+M = length(bgp.pde_setup.δt)
+GPAxis = Axis(resultFigures[2, 4], xlabel = L"$x$ (μm)", ylabel = L"$u(x, t)$ (cells/μm²)", title = @sprintf("(h): PDE curves with sampled ICs\nError: (%.4g, %.4g)", err_CI[1], err_CI[2]), titlealign = :left)
+@views for j in 1:M
+    lines!(GPAxis, bgp.pde_setup.meshPoints * x_scale, soln_vals_mean[:, j] / x_scale^2, color = colors[j])
+    band!(GPAxis, bgp.pde_setup.meshPoints * x_scale, soln_vals_upper[:, j] / x_scale^2, soln_vals_lower[:, j] / x_scale^2, color = (colors[j], 0.35))
+    CairoMakie.scatter!(GPAxis, x_pde[t_pde.==bgp.pde_setup.δt[j]] * x_scale, u_pde[t_pde.==bgp.pde_setup.δt[j]] / x_scale^2, color = colors[j], markersize = 3)
+end
+Legend(resultFigures[1:2, 5], [values(legendentries)...], [keys(legendentries)...], "Time (h)", orientation = :vertical, labelsize = fontsize, titlesize = fontsize, titleposition = :top)
+save("figures/simulation_study_final_quadratic_diffusion_results_basis_approach.pdf", resultFigures, px_per_unit = 2)
