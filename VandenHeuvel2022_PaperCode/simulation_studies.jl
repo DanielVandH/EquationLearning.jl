b20341281819f82e527b2096d30a547497917555
DanielVandH <danj.vandenheuvel@gmail.com>
2022-03-09 19:23:17 +1000

    First data thresholding study

--- VandenHeuvel2022_PaperCode/simulation_studies.jl
+++ VandenHeuvel2022_PaperCode/simulation_studies.jl
@@ -626,7 +626,7 @@ T_params = Vector{Float64}([])
 D_params = [K, 1.0, 1.0, 1.0]
 R_params = [K, 1.0]
 lowers = [0.001, 0.001, 0.001, 1.0]
-uppers = [0.08, 0.08, 0.08,  3.5]
+uppers = [0.08, 0.08, 0.08, 3.5]
 bootstrap_setup = @set bootstrap_setup.B = 10
 bootstrap_setup = @set bootstrap_setup.show_losses = true
 bootstrap_setup = @set bootstrap_setup.Optim_Restarts = 5
@@ -670,7 +670,7 @@ save("figures/simulation_study_revised_quadratic_model_results.pdf", densityPDEF
 
 # Now rescale and perform the full simulation 
 T_params = Vector{Float64}([])
-D_params = [K, 300.0 * t_scale/x_scale^2, 1500.0 * t_scale/x_scale^2, 2779.0 * t_scale/x_scale^2]
+D_params = [K, 300.0 * t_scale / x_scale^2, 1500.0 * t_scale / x_scale^2, 2779.0 * t_scale / x_scale^2]
 R_params = [K, 0.06 * t_scale]
 lowers = [0.99, 0.99, 0.99, 0.8]
 uppers = [1.01, 1.01, 1.01, 1.2]
@@ -744,10 +744,10 @@ save("figures/simulation_study_final_quadratic_diffusion_results.pdf", resultFig
 
 # Now set β₃ = 0
 β₀ = [1.0, 1.0]
-D = (u, β, p) -> β[1] * p[2] + β[2] * p[3] * (u / p[1]) 
-D′ = (u, β, p) -> β[2] * p[3] / p[1] 
+D = (u, β, p) -> β[1] * p[2] + β[2] * p[3] * (u / p[1])
+D′ = (u, β, p) -> β[2] * p[3] / p[1]
 T_params = Vector{Float64}([])
-D_params = [K, 300.0 * t_scale/x_scale^2, 1500.0 * t_scale/x_scale^2]
+D_params = [K, 300.0 * t_scale / x_scale^2, 1500.0 * t_scale / x_scale^2]
 R_params = [K, 0.06 * t_scale]
 lowers = [0.99, 0.99, 0.8]
 uppers = [1.01, 1.01, 1.2]
@@ -850,9 +850,9 @@ gp_setup = EquationLearning.GP_Setup(u; ℓₓ, ℓₜ, σ, σₙ, GP_Restarts, 
 # Define functions and parameters to try and learn 
 T = (t, α, p) -> 1.0
 D = [(u, p) -> 1.0, (u, p) -> u / p[1], (u, p) -> (u / p[1])^2]
-D′ = [(u, p) -> 0.0, (u, p) -> 1 / p[1], (u, p) -> 2u/p[1]^2]
+D′ = [(u, p) -> 0.0, (u, p) -> 1 / p[1], (u, p) -> 2u / p[1]^2]
 R = convert(Vector{Function}, [(u, p) -> u * (1.0 - u / p[1])])
-R′ = convert(Vector{Function}, [(u, p) -> 1.0 - 2u/p[1]])
+R′ = convert(Vector{Function}, [(u, p) -> 1.0 - 2u / p[1]])
 D_params = [K]
 R_params = [K]
 
@@ -925,3 +925,54 @@ GPAxis = Axis(resultFigures[2, 4], xlabel = L"$x$ (μm)", ylabel = L"$u(x, t)$ (
 end
 Legend(resultFigures[1:2, 5], [values(legendentries)...], [keys(legendentries)...], "Time (h)", orientation = :vertical, labelsize = fontsize, titlesize = fontsize, titleposition = :top)
 save("figures/simulation_study_final_quadratic_diffusion_results_basis_approach.pdf", resultFigures, px_per_unit = 2)
+
+#####################################################################
+## Study V: Data thresholding on the Fisher-Kolmogorov model of Study I 
+#####################################################################
+# Generate the data
+pde_setup = @set pde_setup.alg = Tsit5()
+Random.seed!(51021)
+dat = assay_data[1]
+x₀ = dat.Position[dat.Time.==0.0]
+u₀ = dat.AvgDens[dat.Time.==0.0]
+T = (t, α, p) -> 1.0
+D = (u, β, p) -> β[1] * p[1]
+D′ = (u, β, p) -> 0.0
+R = (u, γ, p) -> γ[1] * p[2] * u * (1.0 - u / p[1])
+R′ = (u, γ, p) -> γ[1] * p[2] - 2.0 * γ[1] * p[2] * u / p[1]
+α = Vector{Float64}([])
+β = [310.0] * t_scale / x_scale^2
+γ = [0.044] * t_scale
+T_params = Vector{Float64}([])
+D_params = [1.0]
+R_params = [K, 1.0]
+x, t, u, datgp = EquationLearning.generate_data(x₀, u₀, T, D, R, D′, R′, α, β, γ, δt, finalTime; N, LHS, RHS, alg, N_thin, num_restarts, D_params, R_params, T_params)
+x_pde = copy(x)
+t_pde = copy(t)
+u_pde = copy(u)
+σ = log.([1e-1, 2std(u)])
+σₙ = log.([1e-5, 2std(u)])
+gp, μ, L = EquationLearning.precompute_gp_mean(x, t, u, ℓₓ, ℓₜ, σ, σₙ, nugget, GP_Restarts, bootstrap_setup)
+gp_setup = EquationLearning.GP_Setup(u; ℓₓ, ℓₜ, σ, σₙ, GP_Restarts, μ, L, nugget, gp)
+α₀ = Vector{Float64}([])
+β₀ = [1.0]
+γ₀ = [1.0]
+bootstrap_setup = @set bootstrap_setup.B = 50
+bootstrap_setup = @set bootstrap_setup.show_losses = false
+bootstrap_setup = @set bootstrap_setup.Optim_Restarts = 1
+lowers = [0.7, 0.7]
+uppers = [1.3, 1.3]
+D_params = [310.0] * t_scale / x_scale^2
+R_params = [K, 0.044 * t_scale]
+
+τ₁ = [0.0, 1.0, 2.0, 5.0, 10.0, 15.0] / 100.0
+τ₂ = [0.0, 1.0, 2.0, 5.0, 10.0, 15.0] / 100.0
+errs = zeros(length(τ₁), length(τ₂))
+for (j, τ2) in enumerate(τ₂)
+    for (i, τ1) in enumerate(τ₁)
+        bootstrap_setup = @set bootstrap_setup.τ = (τ1, τ2)
+        bgp = bootstrap_gp(x, t, u, T, D, D′, R, R′, α₀, β₀, γ₀, lowers, uppers; gp_setup, bootstrap_setup, optim_setup, pde_setup, D_params, R_params, T_params, verbose = false)
+        pde_gp = boot_pde_solve(bgp, x_pde, t_pde, u_pde; ICType = "gp")
+        errs[i, j] = error_comp(bgp, pde_gp, x_pde, t_pde, u_pde; compute_mean = true)
+    end
+end
