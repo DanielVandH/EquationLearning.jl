43765328510d21985cbe1c07efa9e26e34f8472f
DanielVandH <danj.vandenheuvel@gmail.com>
2022-04-21 14:58:04 +1000

    more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,12 +6,25 @@ using GaussianProcesses
 using Distributions
 using LinearAlgebra
 using LatinHypercubeSampling
+using OrderedCollections
 using PreallocationTools
+using Setfield
 using DifferentialEquations
-
+using Printf
+using KernelDensity
+using FastGaussQuadrature
+using StructEquality
+using Optim
+using CairoMakie
+using Coverage # using Pkg; Pkg.test("EquationLearning", coverage=true). See https://github.com/JuliaCI/Coverage.jl
+LinearAlgebra.BLAS.set_num_threads(1)
 import Base: ==
 function ==(x::PreallocationTools.DiffCache, y::PreallocationTools.DiffCache)
     x.du == y.du && x.dual_du == y.dual_du
+end
+
+function ==(x::Missing, y::Missing)
+    true
 end
 
 @testset "Utilities" begin
@@ -345,9 +358,9 @@ end
                                 if tt + d + r > 0
                                     lowers = Vector{Float64}([zeros(tt)..., zeros(d)..., zeros(r)...])
                                     uppers = Vector{Float64}([ones(tt)..., ones(d)..., ones(r)...])
-                                    Random.seed!(tt+d+r)
+                                    Random.seed!(tt + d + r)
                                     obj_values, stacked_params, N, Δx, V, Du, D′u, Ru, R′u, TuP, DuP, D′uP, RuP, RuN, SSEArray, Xₛ₀, IC1, initialCondition, MSE = EquationLearning.preallocate_eqlearn(restarts, meshPoints, dt, finalTime, Xₛ, tt, d, r, nₓnₜ, gp, lowers, uppers)
-                                    Random.seed!(tt+d+r)
+                                    Random.seed!(tt + d + r)
                                     bshlper = EquationLearning.bootstrap_helper(x, t, bootₓ, bootₜ, α₀, β₀, γ₀, b, restarts, meshPoints, dt, finalTime, gp, lowers, uppers, z)
                                     @test (x_min, x_max, t_min, t_max, x_rng, t_rng, Xₛ, unscaled_t̃, nₓnₜ,
                                         f, fₜ, fₓ, fₓₓ, ffₜfₓfₓₓ,
@@ -444,4 +457,247 @@ end
     A = zeros(length(point), length(basis))
     EquationLearning.evaluate_basis!(val, coefficients, basis, point, p, A)
     @test val == val2
-end 
+end
+
+@testset "Struct Constructors" begin
+    # Is the GP_Setup constructor working?
+    u = [1.0, 5.0, 0.1, 0.2, 0.5]
+    ℓₓ = log.([1e-6, 1.0])
+    ℓₜ = log.([1e-6, 1.0])
+    σ = log.([1e-6, 7std(u)])
+    σₙ = log.([1e-6, 7std(u)])
+    GP_Restarts = 50
+    μ = [missing, [20.0, 30.0, 40.0]]
+    L = [missing, LowerTriangular{Float64}([20.0 0.0; 10.0 5.0])]
+    nugget = 1e-4
+    Xₛ = [1.0 5.0 10.0 1.0 5.0 10.0 1.0 5.0 10.0
+        0.0 0.0 0.0 1.0 1.0 1.0 2.0 2.0 2.0]
+    gp = [missing, GPE(Xₛ[:, 1:3], ones(3), MeanZero(), SE([0.0, 0.0], 0.0), -2.0)]
+    for m in μ
+        for ℓ in L
+            for gpp in gp
+                gpsetup = GP_Setup(ℓₓ, ℓₜ, σ, σₙ, GP_Restarts, m, ℓ, nugget, gpp)
+                @test GP_Setup(u; ℓₓ, ℓₜ, σ, σₙ, GP_Restarts, μ=m, L=ℓ, nugget, gp=gpp) == gpsetup
+            end
+        end
+    end
+
+    # Is the Bootstrap_Setup constructor working?
+    bootx = [LinRange(0.0, 10.0, 50), LinRange(-10.0, 10.0, 250)]
+    boott = [LinRange(0.0, 5.0, 123), LinRange(0.0, 10.0, 50)]
+    b = [2, 5, 10, 1000]
+    t = [(0.0, 0.0), (0.3, 0.5)]
+    optrestarts = [1, 5]
+    constrain = [false, true]
+    glsscale = [x -> x, x -> 1, log, exp]
+    pdescale = [x -> x, x -> 1, log, exp]
+    initweight = [0.0, 10.0]
+    showlosses = [false, true]
+    for bootₓ in bootx
+        for bootₜ in boott
+            for B in b
+                for τ in t
+                    for Optim_Restarts in optrestarts
+                        for constrained in constrain
+                            for obj_scale_GLS in glsscale
+                                for obj_scale_PDE in pdescale
+                                    for init_weight in initweight
+                                        for show_losses in showlosses
+                                            @test Bootstrap_Setup(rand(10), rand(10); bootₓ, bootₜ, B, τ, Optim_Restarts, constrained, obj_scale_GLS, obj_scale_PDE, init_weight, show_losses) == Bootstrap_Setup(bootₓ, bootₜ, B, τ, Optim_Restarts, constrained, obj_scale_GLS, obj_scale_PDE, init_weight, show_losses)
+                                        end
+                                    end
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+
+    # Is the PDE_Setup constructor working properly?
+    for _ in 1:20
+        meshPoints = LinRange(extrema(10rand(2))..., 500)
+        LHS = rand(3)
+        RHS = rand(3)
+        finalTime = abs.(100rand())
+        r = rand()
+        x = rand(10)
+        t = rand(5)
+        if r < 0.5
+            δt = finalTime / 4
+        else
+            δt = 0:finalTime/4:finalTime
+        end
+        q = rand()
+        if q < 0.5
+            alg = nothing
+        else
+            alg = Tsit5()
+        end
+        @test PDE_Setup(x, t; meshPoints, LHS, RHS, finalTime, δt, alg) == PDE_Setup(meshPoints, LHS, RHS, finalTime, 0:finalTime/4:finalTime, alg)
+    end
+end
+
+@testset "Bootstrapping" begin
+    x_scale = 1000.0
+    t_scale = 24.0
+    alphabet = join('a':'z')
+    fontsize = 20
+    colors = [:black, :blue, :red, :magenta, :green]
+    legendentries = OrderedDict("0" => LineElement(linestyle=nothing, linewidth=2.0, color=colors[1]),
+        "12" => LineElement(linestyle=nothing, linewidth=2.0, color=colors[2]),
+        "24" => LineElement(linestyle=nothing, linewidth=2.0, color=colors[3]),
+        "36" => LineElement(linestyle=nothing, linewidth=2.0, color=colors[4]),
+        "48" => LineElement(linestyle=nothing, linewidth=2.0, color=colors[5]))
+    K = 1.7e-3 * x_scale^2
+    δt = LinRange(0.0, 48.0 / t_scale, 5)
+    finalTime = 48.0 / t_scale
+    N = 1000
+    LHS = [0.0, 1.0, 0.0]
+    RHS = [0.0, -1.0, 0.0]
+    alg = Tsit5()
+    N_thin = 38
+    meshPoints = LinRange(25.0 / x_scale, 1875.0 / x_scale, 500)
+    pde_setup = PDE_Setup(meshPoints, LHS, RHS, finalTime, δt, alg)
+    nₓ = 80
+    nₜ = 75
+    bootₓ = LinRange(25.0 / x_scale, 1875.0 / x_scale, nₓ)
+    bootₜ = LinRange(0.0, 48.0 / t_scale, nₜ)
+    B = 100
+    τ = (0.0, 0.0)
+    Optim_Restarts = 10
+    constrained = false
+    obj_scale_GLS = log
+    obj_scale_PDE = log
+    show_losses = false
+    init_weight = 10.0
+    bootstrap_setup = Bootstrap_Setup(bootₓ, bootₜ, B, τ, Optim_Restarts, constrained, obj_scale_GLS, obj_scale_PDE, init_weight, show_losses)
+    num_restarts = 250
+    ℓₓ = log.([1e-7, 2.0])
+    ℓₜ = log.([1e-7, 2.0])
+    nugget = 1e-5
+    GP_Restarts = 250
+    optim_setup = Optim.Options(iterations=10, f_reltol=1e-4, x_reltol=1e-4, g_reltol=1e-4, outer_f_reltol=1e-4, outer_x_reltol=1e-4, outer_g_reltol=1e-4)
+
+    # Is generate_data working?
+    Random.seed!(51021)
+    x₀ = [0.075, 0.125, 0.175, 0.225, 0.275, 0.325, 0.375, 0.425, 0.475, 0.525, 0.575, 0.625, 0.675, 0.725, 0.775, 0.825, 0.875, 0.925, 0.975, 1.025, 1.075, 1.125, 1.175, 1.225, 1.275, 1.325, 1.375, 1.425, 1.475, 1.525, 1.575, 1.625, 1.675, 1.725, 1.775, 1.825, 1.875]
+    u₀ = [312.0, 261.0, 233.0, 303.0, 252.0, 252.0, 228.0, 242.0, 238.0, 191.0, 135.0, 140.0, 131.0, 79.3, 46.6, 37.3, 46.6, 46.6, 74.6, 65.3, 28.0, 37.3, 14.0, 4.66, 14.0, 0.0, 9.32, 0.0, 28.0, 97.89999999999999, 172.0, 252.0, 368.0, 350.0, 410.0, 331.0, 350.0]
+    T = (t, α, p) -> 1.0
+    D = (u, β, p) -> β[1] * p[1]
+    D′ = (u, β, p) -> 0.0
+    R = (u, γ, p) -> γ[1] * p[2] * u * (1.0 - u / p[1])
+    R′ = (u, γ, p) -> γ[1] * p[2] - 2.0 * γ[1] * p[2] * u / p[1]
+    α = Vector{Float64}([])
+    β = [301.0] * t_scale / x_scale^2
+    γ = [0.044] * t_scale
+    T_params = Vector{Float64}([])
+    D_params = [1.0]
+    R_params = [K, 1.0]
+    x, t, u, datgp = generate_data(x₀, u₀, T, D, R, D′, R′, α, β, γ, δt, finalTime; N, LHS, RHS, alg, N_thin, num_restarts, D_params, R_params, T_params)
+    x_pde = copy(x)
+    t_pde = copy(t)
+    u_pde = copy(u)
+    @test all((mean(x), median(x), sum(x), median(x .^ 2)) .≈ ((0.9878378378378379, 0.9885135135135136, 197.56756756756758, 0.9777076231637043)))
+    @test all((mean(t), median(t), sum(t), median(t .^ 2), median(t .* x)) .≈ ((1.0, 1.0, 200.0, 1.0, 0.7238175675675675)))
+    @test all((mean(u), median(u), median(x .+ t .+ u), sum(u), mean(u .* x), median(u .^ 2 + t .^ 2)) .≈ ((392.6780194026992, 324.3555732722282, 325.90197272833564, 78535.60388053984, 378.7252040143025, 105208.28579627146)))
+
+    # Is precompute_gp_mean working correctly?
+    Random.seed!(51565021)
+    σ = log.([1e-6, 7std(u)])
+    σₙ = log.([1e-6, 7std(u)])
+    gp, μ, L = precompute_gp_mean(x, t, u, ℓₓ, ℓₜ, σ, σₙ, nugget, 250, bootstrap_setup)
+    gp_setup = GP_Setup(u; ℓₓ, ℓₜ, σ, σₙ, GP_Restarts=250, μ, L, nugget, gp)
+    @test gp.mll ≈ -916.4090604839076
+    @test gp.kernel.iℓ2 ≈ [24.84827104335778, 0.503514925015118]
+    @test gp.kernel.σ2 ≈ 470228.28092725377
+    @test gp.logNoise.value ≈ 2.76897258470674
+    @test all((sum(μ), prod(abs.(log.(abs.(μ))) .^ (0.0001)), median(μ), mean(μ), mean(sin.(μ))) .≈ ((2.358682365154418e6, 91.87561481225711, 325.4003821948756, 98.27843188143409, 0.002763346931410305)))
+    @test all((mean(L), median(Symmetric(L)), sum(Symmetric(L)), Symmetric(L)[2, 2], sum(eigen(Symmetric(L)[1:200, 1:200]).values)) .≈ ((0.007124925418994558, 0.0, 10034.10512490004, 3.246503669101823, 28.486899239544083)))
+
+    # Is bootstrap_gp working correctly?
+    Random.seed!(99992001)
+    T = (t, α, p) -> 1.0
+    D = (u, β, p) -> β[1] * p[1]
+    D′ = (u, β, p) -> 0.0
+    R = (u, γ, p) -> γ[1] * p[2] * u * (1.0 - u / p[1])
+    R′ = (u, γ, p) -> γ[1] * p[2] - 2.0 * γ[1] * p[2] * u / p[1]
+    T_params = Vector{Float64}([])
+    D_params = [301.0] * t_scale / x_scale^2
+    R_params = [K, 0.044 * t_scale]
+    α₀ = Vector{Float64}([])
+    β₀ = [1.0]
+    γ₀ = [1.0]
+    lowers = [0.99, 0.9]
+    uppers = [0.99, 1.1]
+    bootstrap_setup = @set bootstrap_setup.B = 20
+    bootstrap_setup = @set bootstrap_setup.show_losses = false
+    bootstrap_setup = @set bootstrap_setup.Optim_Restarts = 2
+    bgp1 = bootstrap_gp(x, t, u, T, D, D′, R, R′, α₀, β₀, γ₀, lowers, uppers; gp_setup, bootstrap_setup, optim_setup, pde_setup, D_params, R_params, T_params, verbose=false)
+    @test size(bgp1.delayBases) == (0, 20)
+    @test bgp1.diffusionBases ≈ Matrix([0.8513680174237094,0.8306630991752704,0.9835242794147602,0.921732832410844,0.9733773297949228,0.9226935020777771,0.7793534011968919,0.8977385045132767,0.8791413689392109,1.0495277282327105,0.9092867495034838,0.8127849598665242,0.980462650553461,0.9303273198133221,0.8430216583525106,0.9850939849138696,0.8851952815813847,1.0023956521556008,0.9860105693350002,0.9218269644490906]')
+    @test bgp1.reactionBases ≈ Matrix([1.001282480560487,0.9929224340035784,1.0020561177614897,1.0036890397229268,1.008784895608166,1.0058788209398541,1.006596786696963,1.0063079736385774,1.0045940703842977,1.0044283567887256,1.0011432979208819,0.9909570426487228,1.0072285264928207,1.0107672368252236,0.995563932496905,1.0001536078456275,1.0165344949319828,1.0036696951245252,1.0168349552135956,1.0148477480794655]')
+    @test bgp1.gp === gp
+    @test mean(bgp1.zvals) ≈ 0.0013533892936884353
+    @test bgp1.zvals[2000, 7] ≈ 0.8946445944917903
+    @test bgp1.Xₛ[1:2, 1:5] ≈ [-0.0277778 -0.0147679 -0.00175809 0.0112518 0.024261; 0.0 0.0 0.0 0.0 0.0] atol = 1e-6
+    @test bgp1.Xₛⁿ[1:2, 4000:4007] ≈ [1.875 0.025 0.0484177 0.0718354 0.0952532 0.118671 0.142089 0.165506; 1.32432 1.35135 1.35135 1.35135 1.35135 1.35135 1.35135 1.35135] atol = 1e-5
+    @test bgp1.bootₓ ≈ collect(bootₓ)
+    @test bgp1.bootₜ ≈ collect(bootₜ)
+    @test bgp1.T === T 
+    @test bgp1.D === D 
+    @test bgp1.D′ === D′ 
+    @test bgp1.R === R 
+    @test bgp1.R′ === R′ 
+    @test bgp1.D_params == Matrix(repeat(D_params, 20)')
+    @test bgp1.R_params ≈ [K K K K K K K K K K K K K K K K K K K K; 0.044*t_scale 0.044*t_scale 0.044*t_scale 0.044*t_scale 0.044*t_scale 0.044*t_scale 0.044*t_scale 0.044*t_scale 0.044*t_scale 0.044*t_scale 0.044*t_scale 0.044*t_scale 0.044*t_scale 0.044*t_scale 0.044*t_scale 0.044*t_scale 0.044*t_scale 0.044*t_scale 0.044*t_scale 0.044*t_scale]
+    @test bgp1.μ ≈ μ
+    @test bgp1.L ≈ L
+    @test bgp1.gp_setup === gp_setup
+    @test bgp1.pde_setup === pde_setup
+    @test bgp1.bootstrap_setup === bootstrap_setup
+
+    # Is density_values working correctly?
+    trv, dr, rr, tt, d, r, delayCIs, diffusionCIs, reactionCIs = density_values(bgp1; level=0.05, diffusion_scales=D_params[1] .* x_scale^2 / t_scale, reaction_scales=R_params[2] / t_scale)
+    @test size(trv) == (0, 20)
+    @test dr ≈ bgp1.diffusionBases * D_params[1] * x_scale^2 / t_scale
+    @test rr ≈ bgp1.reactionBases * R_params[2] / t_scale
+    @test d == 1
+    @test r == 1 
+    @test size(delayCIs) == (0, 2)
+    @test diffusionCIs ≈ [239.365 309.169] atol=1e-3
+    @test reactionCIs ≈ [0.0436432 0.0447345] atol=1e-6
+
+    # Is curve_values working correctly?
+    Tu_vals, Du_vals, Ru_vals, u_vals, t_vals = curve_values(bgp1; level=0.05, x_scale=x_scale, t_scale=t_scale)
+    @test Tu_vals[1] == Tu_vals[2] == Tu_vals[3] == repeat([1.0], 500)
+    @test unique(Du_vals[1]) ≈ [276.10016409823953]
+    @test unique(Du_vals[2]) ≈ [239.36525086105516]
+    @test unique(Du_vals[3]) ≈ [309.1691376209211]
+    @test Ru_vals[1][[1, 10, 59, 100, 250, 397, 500]] ≈ vec([0.102493  1.00685  5.53705  8.81671  16.8485  18.6745  16.3886]) atol=1e-4
+    @test Ru_vals[2][[4, 100, 19, 500, 325]] ≈ [0.40125016693853544, 8.704195093118189, 1.8646702614672126, 16.179425254323252, 18.29139469162819]
+    @test Ru_vals[3][[1, 2, 55, 100, 100, 325]] ≈ [0.10371465515802154, 0.20651772630519405, 5.25407588991313, 8.921838300762065, 8.921838300762065, 18.748760108002376]
+    @test u_vals[[1, 3, 5, 100, 500]] ≈ [2.3216216643979295, 6.9366378754184215, 11.551654086438912, 230.76492410991227, 1153.7681663140106] 
+    @test t_vals[[1, 2, 3, 4, 100, 250, 377]] ≈ [0.0, 0.004008016032064128, 0.008016032064128256, 0.012024048096192385, 0.396793581743487, 0.9979959919839679, 1.5070140280561122]
+
+    # Is compute_initial_conditions working correctly?
+    nodes, weights = gausslegendre(5)
+    tr = bgp1.delayBases
+    dr = bgp1.diffusionBases
+    rr = bgp1.reactionBases
+    B = size(dr, 2)
+    prop_samples = 1.0 
+    rand_pde = convert(Int64, trunc(prop_samples * B))
+    N = length(bgp1.pde_setup.meshPoints)
+    M = length(bgp1.pde_setup.δt)
+    solns_all = zeros(N, rand_pde, M)
+    ICType = "data"
+    initialCondition_all = EquationLearning.compute_initial_conditions(x_pde, t_pde, u_pde, bgp1, ICType)
+
+end
+
+#(:delayBases, :diffusionBases, :reactionBases, :gp, 
+#:zvals, :Xₛ, :Xₛⁿ, :bootₓ, :bootₜ, :T, :D, :D′, :R, :R′, 
+#:D_params, :R_params, :T_params, :μ, :L, :gp_setup, 
+#:bootstrap_setup, :pde_setup)
